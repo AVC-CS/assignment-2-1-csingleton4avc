20633185e4424599abc9c8ac5ad43f31ccd29315
csingleton4avc <csingleton@rhilgames.com>
2023-08-25 19:51:55 -0700

    First Commit

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+#Christopher Singleton
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
+    male_students = int(input('Number of male students: '))
+    female_students = int(input('Number of female students: '))
+    
+    combined_students = male_students + female_students
+    
+    m_perc = ("%.2f%%" % (100 * male_students/combined_students))
+    f_perc = ("%.2f%%" % (100 * female_students/combined_students))
+    
+    print (f'The total number of students: \t \t {combined_students}')
+    print (f'The number of males and females \t {male_students} \t \t {female_students}')
+    print (f'The percentage of males and females \t {m_perc} \t {f_perc}')
+    
 
     """
     ########################################
